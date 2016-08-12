0aec790a652423c3927141a0f63d9cfb11fa989e
unaibot <unaibot@gmail.com>
2016-08-12 11:23:47 +0200

    first commit

---
+++ W1D5.rb
@@ -0,0 +1,35 @@
+#CHESS VALIDATOR
+
+class Board
+	@piece = piece
+	@team = team
+	@board = [
+
+	 ["br"], ["bn"], ["bb"], ["bq"], ["bk"], ["bb"], ["bn"], ["br"]
+	 ["bp"], ["bp"], ["bp"], ["bp"], ["bp"], ["bp"], ["bp"], ["bp"],
+	 ["nil"],["nil"],["nil"],["nil"],["nil"],["nil"]],["nil"],["nil"]
+	 ,["nil"],["nil"],["nil"],["nil"],["nil"],["nil"],["nil"],["nil"]
+	 ["nil"],["nil"],["nil"],["nil"],["nil"],["nil"]],["nil"],["nil"]
+	 ,["nil"],["nil"],["nil"],["nil"],["nil"],["nil"],["nil"],["nil"]
+	  ["wp"], ["wp"], ["wp"], ["wp"], ["wp"], ["wp"], ["wp"], ["wp"],
+	  ["wr"], ["wn"], ["wb"], ["wq"], ["wk"], ["wb"], ["wn"], ["wr"]
+
+
+
+
+
+
+
+class Rook 
+	def initialize team piece
+
+		If a1 
+
+
+
+
+
+
+
+
+
